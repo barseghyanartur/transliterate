1d6eec1d9086e952365d2588146d73a46150a71a
Petar Chakarov <petar@chakarov.info>
2014-07-30 10:27:29 +0300

    Adding Bulgarian language pack

---
+++ src/transliterate/contrib/languages/bg/data/python32.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+mapping = (
+    "abvgdeziyklmnoprstufhaABVGDEZIYKLMNOPRSTUFHA",
+    "абвгдезийклмнопрстуфхъАБВГДЕЗИЙКЛМНОПРСТУФХЪ",
+)
+
+reversed_specific_mapping = (
+    "ь",
+    "y"
+)
+
+pre_processor_mapping = {
+    "zh": "ж",
+    "ts": "ц",
+    "ch": "ч",
+    "sh": "ш",
+    "sht": "щ",
+    "yu": "ю",
+    "ya": "я",
+    "Zh": "Ж",
+    "Ts": "Ц",
+    "Ch": "Ч",
+    "Sh": "Ш",
+    "Sht": "Щ",
+    "Yu": "Ю",
+    "Ya": "Я"
+}
---
+++ src/transliterate/contrib/languages/bg/data/standard.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+
+mapping = (
+    u"abvgdeziyklmnoprstufhaABVGDEZIYKLMNOPRSTUFHA",
+    u"абвгдезийклмнопрстуфхъАБВГДЕЗИЙКЛМНОПРСТУФХЪ",
+)
+
+reversed_specific_mapping = (
+    u"ь",
+    u"y"
+)
+
+pre_processor_mapping = {
+    u"zh": u"ж",
+    u"ts": u"ц",
+    u"ch": u"ч",
+    u"sh": u"ш",
+    u"sht": u"щ",
+    u"yu": u"ю",
+    u"ya": u"я",
+    u"Zh": u"Ж",
+    u"Ts": u"Ц",
+    u"Ch": u"Ч",
+    u"Sh": u"Ш",
+    u"Sht": u"Щ",
+    u"Yu": u"Ю",
+    u"Ya": u"Я"
+}
