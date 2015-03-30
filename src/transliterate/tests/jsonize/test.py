47f4fc35bc8f80e1a3fc8948eb3e9a84611f1b80
Sergey Chernikov <schernikov@gmail.com>
2015-03-30 00:45:24 -0700

    Separate regex's, multichars and translatable single chars

--- src/transliterate/tests/jsonize/test.py
+++ src/transliterate/tests/jsonize/test.py
@@ -7,14 +7,18 @@ import pkgutil, json, os, re
 import transliterate
 import codecs
 import transliterate.contrib.languages as langs
-from transliterate.exceptions import LanguagePackNotFound, ImproperlyConfigured
+from transliterate.exceptions import LanguagePackNotFound
 
 resub = re.compile('\{(?P<sub>[^\}]+)\}', re.UNICODE)
 regroup = re.compile('\[(?P<list>[^\]]+)\]', re.UNICODE)
+recontrol = re.compile('[\x00-\x1f]', re.UNICODE)
+reslash = re.compile(r'(?P<ctrl>\\{1,1}[^\\])', re.UNICODE)
+pycontrols = (r'\A', r'\b', r'\B', r'\Z')
 
 def main():
     ru = translator('ru')
-    ru
+    codec = ru.codec()
+    codec.show()
     #===========================================================================
     # for shorts in ru['latin']['shortcuts']:
     #     name = shorts['name']
@@ -48,6 +52,8 @@ def translator(language_code, script_name='latin', variant_name=None):
     variant.unshort()
 
     variant.ungroup()
+    
+    variant.uncontrol()
 
     return variant
 
@@ -56,7 +62,7 @@ def findmatches(variant, chars, script_name):
     mm = []
     for ch in chars:
         mchar = variant.get(ch, None)
-        if not mchar: raise ImproperlyConfigured("Can not find matching char for %s in %s" % (ch, script_name))
+        if not mchar: raise InvalidFormat("Can not find matching char for %s in %s" % (ch, script_name))
         if type(mchar) == list:
             mm.append(mchar[0])
         else:
@@ -81,7 +87,7 @@ class Language(object):
     
         scr = tr.get(sname, None)
         if not scr:
-            raise ImproperlyConfigured("Language script %s from  %s pack is missing" % (sname, self.name))
+            raise InvalidFormat("Language script %s from  %s pack is missing" % (sname, self.name))
         
         return Script(self, scr, sname)
         
@@ -91,22 +97,26 @@ class Script(object):
     def __init__(self, lang, scr, name):
         self._lang = lang
         self._script = scr
-        self.name = name
+        self._name = name
+        
+    @property
+    def name(self):
+        return "%s[%s]"%(self._lang.name, self._name)
         
     def variant(self, vname):
         if not vname:
             vname = self._script.get('default', None)
     
         if not vname:
-            raise ImproperlyConfigured("Language script %s from %s pack is missing; default variant is not defined" % (self.name, self._lang.name))
+            raise InvalidFormat("Language script %s from %s pack is missing; default variant is not defined" % (self.name, self._lang.name))
     
         vv = self._script.get('variants', None)
         if vv is None:
-            raise ImproperlyConfigured("Language script %s from %s pack is missing variants definitions. Expecting %s" % (self.name, self._lang.name, vname))
+            raise InvalidFormat("Language script %s from %s pack is missing variants definitions. Expecting %s" % (self.name, self._lang.name, vname))
         
         v = vv.get(vname, None)
         if v is None:
-            raise ImproperlyConfigured("Language script %s from  %s pack is missing expected variant %s" % (self.name, self._lang.name, vname))
+            raise InvalidFormat("Language script %s from  %s pack is missing expected variant %s" % (self.name, self._lang.name, vname))
         
         return Variant(self, v, vname)
     
@@ -148,7 +158,7 @@ class Variant(object):
             if m:
                 sub = m.groupdict()['sub']
                 sh = shorts.get(sub, None)
-                if sh is None: raise ImproperlyConfigured("Undefined shortcut %s in %s"%(sub, self._script.name))
+                if sh is None: raise InvalidFormat("Undefined shortcut %s in %s"%(sub, self._script.name))
                 start, end = m.regs[0]
                 ss.append((s[start:end], sh))
     
@@ -163,15 +173,15 @@ class Variant(object):
         for k, v in self._var.items():
             kgs = ungroup(k)
             if type(v) == list:
-                if len(kgs) != 0: raise ImproperlyConfigured("Can not have groups and lists in the pair (%s, %s) in %s" % (k, v, self._script.name))
+                if len(kgs) != 0: raise InvalidFormat("Can not have groups and lists in the pair (%s, %s) in %s" % (k, v, self._script.name))
                 newvars[k] = v
                 continue
             vgs = ungroup(v)
             if len(kgs) == 0 and len(vgs) == 0:
                 newvars[k] = v 
                 continue
-            if len(kgs) < len(vgs): raise ImproperlyConfigured("Groups are not matching for %s and %s in %s" % (k, v, self._script.name))
-            if len(kgs) > 1: raise ImproperlyConfigured("Only one group per entry is supported. Found %d in %s from %s" % (len(kgs), k, self._script.name))
+            if len(kgs) < len(vgs): raise InvalidFormat("Groups are not matching for %s and %s in %s" % (k, v, self._script.name))
+            if len(kgs) > 1: raise InvalidFormat("Only one group per entry is supported. Found %d in %s from %s" % (len(kgs), k, self._script.name))
             kg = kgs[0]
             if len(vgs) == 0:
                 self.unroll(newvars, k, kg, lambda idx: v)
@@ -181,7 +191,7 @@ class Variant(object):
                 vg.set = findmatches(self._var, kg.set, self._script.name)
                 
             if len(kg.set) != len(vg.set):
-                raise ImproperlyConfigured("Groups must match in %s and %s from %s" % (k, v, self._script.name))
+                raise InvalidFormat("Groups must match in %s and %s from %s" % (k, v, self._script.name))
             
             pre = v[:vg.start]
             post = v[vg.stop:]
@@ -192,10 +202,22 @@ class Variant(object):
 
         self._var = newvars
 
-        #TODO: remove        
+    def uncontrol(self):
         for k, v in self._var.items():
-            print k, v
-        #
+            self._checkcontr(k)
+            
+            if type(v) == list:
+                for sv in v:
+                    self._checkcontr(sv)
+            else:
+                self._checkcontr(v)
+
+    def _checkcontr(self, val):
+        m = recontrol.search(val)
+        if m: raise InvalidFormat("No control chars are allowed. Got %s in %s. Use extra '\\' in json"%(val, self._script.name))
+        for m in reslash.finditer(val):
+            ctrl = m.groupdict()['ctrl']
+            if not (ctrl in pycontrols): raise InvalidFormat("Only '%s' controls are allowed. Got '%s'"%(pycontrols, ctrl))
         
     def unroll(self, newvars, k, kg, onv):
         pre = k[:kg.start]
@@ -208,15 +230,17 @@ class Variant(object):
                 newvars[nk] = v
             else:
                 if nv != v:
-                    raise ImproperlyConfigured("Warning: uncontrolled order of options %s or %s for %s in %s"%(nv, v, nk, self._script.name))
+                    raise InvalidFormat("Warning: uncontrolled order of options %s or %s for %s in %s"%(nv, v, nk, self._script.name))
                 
     def codec(self):
         mp = {}
         for k, v in self._var.items():
-            if type(v) == list:
-                mp[k] = v[0]
-            else:
-                mp[k] = v
+            if type(v) == list: v = v[0]
+            subs = set(reslash.findall(v))
+            if subs: # clean-out all escaped controls from target script
+                for s in subs:
+                    v = v.replace(s, '')
+            mp[k] = v
 
         return Codec(mp)
 
@@ -224,10 +248,51 @@ class Variant(object):
 class Codec(object):
 
     def __init__(self, mp):
-        self._map = mp
+        regs = []
+        orders = {}
+        singles = {}
+        for k, v in mp.items():
+            if reslash.search(k): # has regex controls
+                regs.append((re.compile(k, re.UNICODE), v))
+                continue
+            if len(k) == 1:
+                singles[k] = v
+                continue
+            ordr = orders.get(len(k), None)
+            if ordr is None:
+                ordr = {}
+                orders[len(k)] = ordr
+            ordr[k] = v
+        maps = [orders[sz] for sz in sorted(orders.keys(), reverse=True)]
+        self._regs = tuple(regs)
+        self._subs = tuple(maps)
+        self._trans = singles
 
     def transcode(self, msg):
+        #TODO finish
         return msg
+    
+    def show(self):
+        if self._regs:
+            print "Regexes:"
+            for rr, v in self._regs:
+                print "  %s -> %s"%(rr.pattern, v)
+            print
+        if self._subs:
+            print "Substitutes:"
+            for sub in self._subs:
+                kk = sub.keys()
+                print "  size %d:"%(len(kk[0]))
+                for k in sorted(kk):
+                    print "    %s -> %s"%(k, sub[k])
+                print
+
+        if self._trans:
+            print "Translates:"
+            for k in sorted(self._trans.keys()):
+                print "  %s -> %s"%(k, self._trans[k])
+            print 
+
 
 class Group(object):
     
@@ -267,6 +332,11 @@ def checkmapping(loc, name, lat, scr):
     with codecs.open(os.path.join(loc, '%s.gen.json'%name), 'wb', 'utf-8') as f:
         json.dump({'base':subscr, 'latin':latscr}, f, ensure_ascii=False, separators=(',',':'))
     
+    
+class InvalidFormat(Exception):
+    
+    def __init__(self, msg):
+        super(InvalidFormat, self).__init__(msg)
     
 if __name__ == '__main__':
     main()
