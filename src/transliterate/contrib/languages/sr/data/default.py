0b89adeabb2647d0d64f5cc2cced9ae43e74e65d
sasa <saskelecevic@gmail.com>
2017-01-08 18:52:53 +0100

    Serbian language pack - initial data

---
+++ src/transliterate/contrib/languages/sr/data/default.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+mapping = (
+    u"abvgdđežzijklmnoprstćufhcčšABVGDĐEŽZIJKLMNOPRSTĆUFHCČŠ",
+    u"абвгдђежзијклмнопрстћуфхцчшАБВГДЂЕЖЗИЈКЛМНОПРСТЋУФХЦЧШ",
+)
+
+reversed_specific_mapping = (
+    u"",
+    u""
+)
+
+pre_processor_mapping = {
+    u"lj": u"љ",
+    u"nj": u"њ",
+    u"dž": u"џ",
+    u"Lj": u"Љ",
+    u"Nj": u"Њ",
+    u"Dž": u"Џ",
+}
