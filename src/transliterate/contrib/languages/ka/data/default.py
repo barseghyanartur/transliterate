daa6137699a27e2f75be774b54e4bd820ec21dc3
Nodo Okroshiashvili <n.okroshiashvili@gmail.com>
2018-04-23 13:20:40 +0400

    Update default.py

--- src/transliterate/contrib/languages/ka/data/default.py
+++ src/transliterate/contrib/languages/ka/data/default.py
@@ -2,22 +2,22 @@
 
 mapping = (
     u"abgdevztiklmnoprsufqycxjhwABGDEVIKLMNOPUFQYXH",
-    u"აბგდევზთიკლმნოპრსუფქყცხჯჰწაბგდებიკლმნოპუფქყხჰ",
+    u"აბგდევზთიკლმნოპრსუფქყცხჯჰწაბგდევიკლმნოპუფქყხჰ",
 )
 
 
 
 
 pre_processor_mapping = {
-    u"zh'": u"ჟ",
+    u"zh": u"ჟ",
     u"T'": u"ტ",
     u"gh": u"ღ",
     u"sh": u"შ",
     u"ch": u"ჩ",
-    u"W'": u"ჭ",
+    u"W": u"ჭ",
     u"ch'": u"ჭ",
     u"dz": u"ძ",
-    u"ts'": u"წ",
+    u"ts": u"წ",
     u"kh": u"ხ",
     u"R'": u"ღ",
     u"S'": u"შ",
