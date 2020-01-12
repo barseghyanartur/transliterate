b21dab925f56bd2f6e220a8eb9e5075a8d2eddb7
Piotr Herbut <piotr.herbut@gmail.com>
2020-01-12 21:57:56 +0100

    handle soft sign after vovel

--- src/transliterate/__main__.py
+++ src/transliterate/__main__.py
@@ -1,8 +1,15 @@
 from transliterate.discover import autodiscover
 from transliterate import translit, get_available_language_codes
+from functools import partial
+import os
+
+autodiscover()
+
+_to_latin = partial(translit, language_code='lem', reversed=True)
+_file_path = os.path.join('/', 'home', 'petro', 'dev', 'priv', 'lem-spiwnyk', 'spysany', 'boze_boze_jakij_to_zal.tex')
 
 if __name__ == '__main__':
-    autodiscover()
-    print(get_available_language_codes())
     text = 'Лемковино, краю рідний'
-    print(translit(text, 'lem', reversed=True))
+    with open(_file_path, 'r') as f:
+        content = f.read()
+        print(_to_latin(content))
